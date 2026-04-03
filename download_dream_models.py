9a4cfcea46aba20f3dc1e2bb961966a38b52cbcc
Justice Sefas <jsefas@cs.ubc.ca>
2026-04-02 20:50:26 -0400

    add llada models

--- download_dream_models.py
+++ download_dream_models.py
@@ -8,22 +8,39 @@ for f in files:
     if f.endswith('ae.pt') or f.endswith('config.json'):
         path = hf_hub_download(
             repo_id="AwesomeInterpretability/dlm-mask-topk-sae",
-            local_dir='saes',
+            local_dir='dream_saes',
             filename=f
         )
 
-model_path = "Dream-org/Dream-v0-Base-7B"
-model = AutoModel.from_pretrained(
-    model_path,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True
-)
+files = list_repo_files("AwesomeInterpretability/llada-mask-topk-sae")
 
-model.save_pretrained('dream')
+for f in files:
+    if f.endswith('ae.pt') or f.endswith('config.json'):
+        path = hf_hub_download(
+            repo_id="AwesomeInterpretability/llada-mask-topk-sae",
+            local_dir='llada_saes',
+            filename=f
+        )
 
-tokenizer = AutoTokenizer.from_pretrained(
-    model_path,
-    trust_remote_code=True
-)
+model_paths = ["Dream-org/Dream-v0-Base-7B", "GSAI-ML/LLaDA-8B-Base"]
+model_names = ["dream", "llada"]
 
-tokenizer.save_pretrained('dream_tokenizer')
+model_paths = ["GSAI-ML/LLaDA-8B-Base"]
+model_names = ["llada"]
+
+for model_path, model_name in zip(model_paths, model_names):
+    model = AutoModel.from_pretrained(
+        model_path,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True
+    )
+    
+    model.save_pretrained(model_name)
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        trust_remote_code=True
+    )
+    
+    tokenizer_name = model_name + '_tokenizer'
+    tokenizer.save_pretrained(tokenizer_name)
